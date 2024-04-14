2abbf84f938745e3eb4291aa4770b74d2a6aa49c
Jorn Lotsch <j.lotsch@em.uni-frankfurt.de>
2024-04-14 13:50:02 +0000

    version 1.0

--- R/MakeReducedDataMat.R
+++ R/MakeReducedDataMat.R
@@ -1,24 +1,32 @@
-# Performs random class-proportional downsampling and compares the sampled and
-# original distributions
+#' Perform Random Class-Proportional Downsampling and Compare Distributions
+#'
+#' This function performs random class-proportional downsampling on a dataset and
+#' compares the sampled and original distributions using a specified statistical test.
+#'
+#' @param DataAndClasses A data frame containing the data and class labels.
+#' @param Size A numeric value between 0 and 1 representing the desired size of the
+#'   downsampled dataset as a proportion of the original dataset.
+#' @param Seed An integer value to be used as the random seed for reproducibility.
+#'
+#' @return A list with the following elements:
+#'   - `ReducedDataList`: The downsampled dataset.
+#'   - `RemovedDataList`: The removed data from the original dataset.
+#'
 #' @importFrom caTools sample.split
-MakeReducedDataMat <-
-  function(DataAndClasses, TestStat, Size, Seed) {
-    set.seed(Seed)
-    sample <-
-      caTools::sample.split(DataAndClasses$Cls, SplitRatio = Size / nrow(DataAndClasses))
-    ReducedDataList <- subset(DataAndClasses, sample == TRUE)
-    RemovedDataList <- subset(DataAndClasses, sample == FALSE)
-    ADv <-
-      mapply(CompDistrib,
-             vector1 = DataAndClasses[1:(ncol(DataAndClasses) - 1)],
-             vector2 = ReducedDataList[1:(ncol(ReducedDataList) -
-                                            1)],
-             TestStat)
-    return(
-      list(
-        ReducedDataList = ReducedDataList,
-        RemovedDataList = RemovedDataList,
-        ADv = ADv
-      )
-    )
-  }
+#'
+MakeReducedDataMat <- function(DataAndClasses, Size, Seed) {
+  # Set the random seed for reproducibility
+  set.seed(Seed)
+  
+  # Perform class-proportional downsampling
+  sample <- caTools::sample.split(DataAndClasses$Cls, SplitRatio = Size)
+  ReducedDataList <- DataAndClasses[sample, ]
+  RemovedDataList <- DataAndClasses[!sample, ]
+  
+  return(list(
+    ReducedDataList = ReducedDataList,
+    RemovedDataList = RemovedDataList
+  ))
+}
+
+
